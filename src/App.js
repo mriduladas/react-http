480bd0b189c7ab30213faa1258bc829391d872ca
mriduladas <mridula199@gmail.com>
2022-04-23 23:18:24 +0530

    Handle Error

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   // const dummyMovies = [];
   const [movies, setMovies]= useState([]);
   const [isLoading, setIsLoading]= useState(false);
+  const [error, setError] = useState(null);
 
   // function fetchMovies(){
   //   fetch('https://swapi.py4e.com/api/films')
@@ -25,34 +26,47 @@ function App() {
   //     setMovies(transformedMovies);
   //   })
   // }
+
+  //This download the json to PC
   const handleSaveToPC = (jsonData,filename) => {
-    const fileData = JSON.stringify(jsonData);
-    const blob = new Blob([fileData], {type: "text/plain"});
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.download = `${filename}.json`;
-    link.href = url;
-    link.click();
+    //Uncomment to download
+    // const fileData = JSON.stringify(jsonData);
+    // const blob = new Blob([fileData], {type: "text/plain"});
+    // const url = URL.createObjectURL(blob);
+    // const link = document.createElement('a');
+    // link.download = `${filename}.json`;
+    // link.href = url;
+    // link.click();
   }
 
   async function fetchMovies(){
     setIsLoading(true);
-    const response = await fetch('https://swapi.py4e.com/api/films')
-    const data = await response.json();
-   
-    const transformedMovies = data.results.map((movieData) =>{
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }}
+    setError(null);
+    try{
+      const response = await fetch('https://swapi.py4e.com/api/films')
+      if(!response.ok){
+        throw new Error('Something went wrong')
+      }
+      const data = await response.json();
+     
+      const transformedMovies = data.results.map((movieData) =>{
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date
+          }}
+          
+        );
+        console.log(transformedMovies)
+        handleSaveToPC(data,'abcd');
+        setMovies(transformedMovies);
         
-      );
-      console.log(transformedMovies)
-      handleSaveToPC(data,'abcd');
-      setMovies(transformedMovies);
-      setIsLoading(false);
+    }catch(error){
+      setError(error.message);
+    }
+    setIsLoading(false);
+
   }
 
   return (
@@ -62,8 +76,9 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length>0  && <MoviesList movies={movies} />}
-        {!isLoading && movies.length===0 && <p>No Movies Found</p>}
+        {!isLoading && movies.length===0 && !error && <p>No Movies Found</p>}
         {isLoading && <p>Loading..</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
